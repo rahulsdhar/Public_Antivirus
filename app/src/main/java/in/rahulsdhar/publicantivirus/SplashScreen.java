44604a12b72c3e322ebeb56919c1b3699d949acf
rahulsdhar <rahulsdhar51@gmail.com>
2020-03-20 16:06:50 +0530

    init

---
+++ app/src/main/java/in/rahulsdhar/publicantivirus/SplashScreen.java
@@ -0,0 +1,14 @@
+package in.rahulsdhar.publicantivirus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
